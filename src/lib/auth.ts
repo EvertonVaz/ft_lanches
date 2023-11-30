5984abe342d45274ec100fdb7f1a5cee329f4a56
Everton Vaz <56730329+EvertonVaz@users.noreply.github.com>
2023-11-30 20:13:57 +0000

    testando outro nome nas dontenv

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,8 @@ import FortyTwoProvider from 'next-auth/providers/42-school'
 export const authOptions: NextAuthOptions = {
   providers: [
     FortyTwoProvider({
-      clientId: process.env.FORTY_TWO_CLIENT_ID as string,
-      clientSecret: process.env.FORTY_TWO_CLIENT_SECRET as string,
+      clientId: process.env.UID as string,
+      clientSecret: process.env.SECRET as string,
     }),
   ],
 }
